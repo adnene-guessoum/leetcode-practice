90964ec270cc2f4c93e9e12160c869dcdb79509e
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-11 11:50:18 +0100

    Add new failing tests
    test: compare if first letters are same for each element of list

--- 14_LongestCommonPrefix/test_solution.py
+++ 14_LongestCommonPrefix/test_solution.py
@@ -1,6 +1,15 @@
 from solution import *
 
-def test_return_first_letter_each_element():
+def passed_test_return_first_letter_each_element():
 	sol = Solution()
 	assert sol.longestCommonPrefix(["tree", "table", "pool"]) == ["t", "t", "p"]
+
+
+def test_compare_first_letter_each_element_false():
+	sol = Solution()
+	assert sol.longestCommonPrefix(["tree", "table", "pool"]) == False
+
+def test_compare_first_letter_each_element_true():
+	sol = Solution()
+	assert sol.longestCommonPrefix(["foot", "fall", "forever"]) == True
 
