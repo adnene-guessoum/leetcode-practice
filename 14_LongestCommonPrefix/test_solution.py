59115198d60bfaad8e1183d43035f7aa30939b7b
Adnene_wsl <adnen.guessoum@gmail.com>
2022-11-11 12:12:23 +0100

    Two letter test

--- 14_LongestCommonPrefix/test_solution.py
+++ 14_LongestCommonPrefix/test_solution.py
@@ -4,7 +4,6 @@ def passed_test_return_first_letter_each_element():
 	sol = Solution()
 	assert sol.longestCommonPrefix(["tree", "table", "pool"]) == ["t", "t", "p"]
 
-
 def passed_test_compare_first_letter_each_element_false():
 	sol = Solution()
 	assert sol.longestCommonPrefix(["tree", "table", "pool"]) == False
@@ -13,12 +12,17 @@ def passed_test_compare_first_letter_each_element_true():
 	sol = Solution()
 	assert sol.longestCommonPrefix(["foot", "fall", "forever"]) == True
 
-def test_return_letter_if_true():
+def passed_test_return_letter_if_true():
 	sol = Solution()
 	assert sol.longestCommonPrefix(["foot", "fall", "forever"]) == "f"
 
-def test_return_empty_string_if_false():
+def passed_test_return_empty_string_if_false():
 	sol = Solution()
 	assert sol.longestCommonPrefix(["tree", "table", "pool"]) == ""
+
+def test_output_for_two_letter_prefix():
+	sol = Solution()
+	assert sol.longestCommonPrefix(["tree", "trunk", "triangle"]) == "tr"
+
 
 
