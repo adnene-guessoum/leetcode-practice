9a80a7886ffe53a14697a3a8dabe401ad418eb8b
Adnene_wsl <adnen.guessoum@gmail.com>
2023-01-01 22:26:55 +0100

    53_MaximumSubarray_unsolved

---
+++ medium/53_MaximumSubarray.py
@@ -0,0 +1,10 @@
+# thoughts
+
+"""
+initial idea:
+	go through every subarray and save the sum for comparisons...	
+
+	start from beginning and dismiss numbers that don't contribute to
+	improving the sum ?
+
+"""
